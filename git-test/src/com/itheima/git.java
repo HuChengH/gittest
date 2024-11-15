1ed4004a0eb88012ef284733347c0bf3031d2d60
huchenghao <13425081+huchenghao123@user.noreply.gitee.com>
2024-11-15 16:00:33 +0800

    测试本地工程

---
+++ git-test/src/com/itheima/git.java
@@ -0,0 +1,20 @@
+package com.itheima;
+
+public class git {
+    public static void main(String[] args){
+        System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("hello git5");
+        System.out.println("hello git6");
+        System.out.println("hello git7");
+        System.out.println("hello git8");
+        System.out.println("hello git9");
+        System.out.println("hello git10");
+        System.out.println("hello git11");
+        System.out.println("hello git12");
+        System.out.println("hello git13");
+        System.out.println("hello git14");
+    }
+}
